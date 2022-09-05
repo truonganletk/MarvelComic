7ce1093ada82cad202bf17289b2cc456f2e55775
ndtuan7921 <ndtuan7921@gmail.com>
2022-09-06 01:13:35 +0700

    header not responsive

--- src/layouts/Header/Header.jsx
+++ src/layouts/Header/Header.jsx
@@ -1,9 +1,60 @@
 import React, { Component } from 'react'
+import { ReactComponent as MarvelLogo} from '../../assets/images/header_logo.svg'
+import iconMUshield from '../../assets/images/icon-mu-shield.png'
+import { ReactComponent as SearchIcon } from '../../assets/images/search-svgrepo-com.svg'
 
 export class Header extends Component {
   render() {
     return (
-      <div>header</div>
+      <>
+        <header className='header'>
+          <div className="header__upper">
+            <div className="upperContainer">
+              <div className="user-menu-tab">
+                <div className="user-menu__avatar">
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 65.6 66.7">
+                    <path fill-rule="evenodd" d="M59.5 15.1L53 51.4c0 .4-.3.6-.6.6h-6.5c-.3 0-.6-.2-.8-.6l-8.8-24.7H36l-4.3 24.7c-.1.3-.3.6-.6.6H25c-.3 0-.6-.2-.5-.6l6.6-37.3c.1-.3.3-.6.6-.6h6.4c.5 0 .7.2.8.6l8.8 24.7h.3l5.5-30.4C47.9 3.7 40.6.9 32.7.9 14.7.9.2 15.5.2 33.4c0 8.8 3.5 16.7 9.1 22.5l7.3-41.7c.1-.3.3-.6.6-.6H24c.3 0 .5.2.5.6l-8.3 47.2c4.8 2.8 10.4 4.5 16.4 4.5 17.9 0 32.5-14.5 32.5-32.5 0-6.8-2.1-13.1-5.6-18.3z">
+                    </path>
+                  </svg>
+                </div>
+                <div className="user-menu-links">
+                  <a href="/#" className="user-menu__sign-in">Sign in</a>
+                  <span>|</span>
+                  <a href="/#" className="user-menu__join">Join</a>
+                </div>
+              </div>
+              <a href="/" className="logo">
+                <MarvelLogo/>
+              </a>
+              <div className="right-links">
+                <div className="searchPromo-Container">
+                  <div className="searchPromo__img">
+                    <img src={ iconMUshield } alt="icon-mu-shield"/>
+                  </div>
+                  
+                  <div className="searchPromo__wrap">
+                    <p>Marvel Unlimited</p>
+                    <span>Subscribe</span>
+                  </div>
+                </div>
+                <div className="search">
+                  <a href="/search" className="search-link">
+                    <SearchIcon className='search_icon'/>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="header__lower">
+            <ul>
+              <li><a href="/">HOME</a></li>
+              <li><a href="/category">CATEGORIES</a></li>
+              <li><a href="/detail">DETAILS</a></li>
+              <li><a href="/search">SEARCH</a></li>
+            </ul>
+          </div>
+        </header>
+      </>
     )
   }
 }
