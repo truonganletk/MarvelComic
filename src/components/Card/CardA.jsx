54b1579de4eeb16592c5d965e03c0a8e083fd16d
ndtuan7921 <ndtuan7921@gmail.com>
2022-09-12 00:37:25 +0700

    listA

---
+++ src/components/Card/CardA.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import MuIcon from '../../assets/images/mu-icon-dark-bg.png' 
+
+const CardA = ({data}) => {
+    return (
+        <>
+            <div className="cardA-content">
+                <div className="cardA-image">
+                    <img src={`${data.thumbnail.path}.${data.thumbnail.extension}`} alt="" className='thumbnail'/>
+                    <img src={MuIcon} alt="" className='mu-icon-dark-bg'/>
+                </div>
+                <div className="cardA-text">
+                    <h5 className='cardA-title'>
+                        { data.title }
+                    </h5>
+                    <p className='creators'>
+                       
+                    </p>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default CardA
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Footer from '../layouts/Footer/Footer'
 import Header from '../layouts/Header/Header'
+import ListA  from '../layouts/ListA/ListA'
 
 
 export class home extends Component {
@@ -9,6 +10,7 @@ export class home extends Component {
       <>
         <Header/>
         home
+        <ListA/>
         <Footer/>
       </>
     )
