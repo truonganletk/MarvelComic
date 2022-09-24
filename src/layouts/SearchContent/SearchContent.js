b02716af41c5f519a799de0ffa223c1c442ce680
truonganletk <truonganletk@gmail.com>
2022-09-24 11:59:15 +0300

    search

--- src/layouts/SearchContent/SearchContent.js
+++ src/layouts/SearchContent/SearchContent.js
@@ -1,14 +1,18 @@
+import { faSearchengin } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { useEffect, useState } from 'react'
 import {
-    useLocation,
+    // useLocation,
     useNavigate, createSearchParams
 } from "react-router-dom";
+import ListC from '../ListC/ListC';
 
 
 const SearchContent = () => {
     const [limit] = useState(20);
-    const [contentType, setContentType] = useState("");
-    const [nameStart] = useState("");
+    const [contentType, setContentType] = useState("Characters");
+    const [nameStart, setNameStart] = useState("");
+    const [loadList, setLoadList] = useState(false);
 
     const navigate = useNavigate();
 
@@ -24,21 +28,38 @@ const SearchContent = () => {
                 }).toString()
             });
         };
+        if (nameStart !== "") {
+            setLoadList(false);
+            setLoadList(true);
+        }
         navigateHandler();
     }, [limit, contentType, nameStart, navigate])
 
-
-    const { search } = useLocation();
+    const handleKeyPress = (e) => {
+        if (e.key === 'Enter') {
+            setNameStart(e.target.value);
+        }
+    }
+    // const { search } = useLocation();
     return (
-        <>
-            <div>Query search: {search}</div>
-            <button onClick={() => setContentType("Characters")}>
-                Characters
-            </button>
-            <button onClick={() => setContentType("Comics")}>
-                Comics
-            </button>
-        </>
+        <div className='search-content'>
+            {/* <div>Query search: {search}</div> */}
+            <div className='search-block'>
+                <div className='search-icon'><FontAwesomeIcon icon={faSearchengin} /></div>
+                <input type={"text"} placeholder="SEARCH" className="search-input" onKeyPress={(e) => { handleKeyPress(e) }} />
+            </div>
+            <ul>
+                <li className={`search-btn ${contentType === "Characters" ? "active" : ""}`} onClick={() => setContentType("Characters")}>
+                    Characters
+                </li>
+                <li className={`search-btn ${contentType === "Comics" ? "active" : ""}`} onClick={() => setContentType("Comics")}>
+                    Comics
+                </li>
+            </ul>
+            {
+                loadList ? <ListC nameStartWith={nameStart} /> : <></>
+            }
+        </div>
     );
 };
 
