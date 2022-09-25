fe20ce6602ea8f8dff2a3c724e7d2d5626e703bb
truonganletk <truonganletk@gmail.com>
2022-09-25 13:49:01 +0300

    fix comic detail description

--- src/layouts/Comic/ComicDetail.jsx
+++ src/layouts/Comic/ComicDetail.jsx
@@ -32,7 +32,7 @@ function ComicDetail(props) {
                             <Title1 title="Cover Artist:" subtitle="Lorem" />
                         </div>
                         <p className='Comiccard-text-description Comiccard-text-flex2'>
-                            {(props.data.description !== '' ? props.data.description : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")}
+                            {(props.data.description !== ''&&props.data.description !== null ? props.data.description : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")}
                         </p>
                         <Link to="/#"><span className='Comiccard-text-button'>See Variant Covers</span></Link>
                     </div>
