75a72c9638357611617ad8235362f48155d5c61f
An Le <truonganletk@gmail.com>
2022-09-28 00:39:30 +0300

    scroll to top on route change

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import './assets/scss/main.scss';
 import {
   BrowserRouter as Router,
   Routes,
-  Route
+  Route,
+  useLocation
 } from "react-router-dom";
 import Comic from './pages/Comic';
 import Home from './pages/Home';
@@ -11,20 +12,31 @@ import Characters from './pages/Characters';
 import Character from './pages/Character';
 import Comics from './pages/Comics';
 import Page404 from './pages/Page404';
+import { useLayoutEffect } from 'react';
+
+
+const Wrapper = ({ children }) => {
+  const location = useLocation()
+  useLayoutEffect(() => {
+    document.documentElement.scrollTo(0, 0)
+  }, [location.pathname])
+  return children
+}
 
 function App() {
   return (
     <Router>
+      <Wrapper>
         <Routes>
-          <Route exact path="/characters" element={<Characters/>} />
-          <Route exact path="/comics" element={<Comics/>} />
-          <Route path="/search" element={<Search/>} />    
-          <Route path="/comic/:id" element={<Comic/>} />
-          <Route path="/character/:id" element={<Character/>} />
-          <Route path="/" element={<Home/>} />
-          <Route path="*" element={<Page404/>} />
-
+          <Route exact path="/characters" element={<Characters />} />
+          <Route exact path="/comics" element={<Comics />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/comic/:id" element={<Comic />} />
+          <Route path="/character/:id" element={<Character />} />
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<Page404 />} />
         </Routes>
+      </Wrapper>
     </Router>
   );
 }
